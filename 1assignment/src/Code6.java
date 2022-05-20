d070af95e426bb03abc960521b7ce7a52f888486
STUDENT27 <STUDENT27@SJ-PC27>
2022-05-20 14:04:19 +0530

    program6

---
+++ 1assignment/src/Code6.java
@@ -0,0 +1,13 @@
+
+public class Code6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i= 10;  // variable with f is a type of integer which is holding value of 10
+	       long l = (long) i; 
+	System.out.println(l);
+
+
+	}
+
+}
