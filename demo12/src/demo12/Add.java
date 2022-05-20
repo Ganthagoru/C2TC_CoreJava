f71426ecee0a4a99a1fdc94cb8037366e2eb701a
91868 <91868@LAPTOP-CVF97MDI>
2022-05-20 10:04:04 +0530

    third commit

---
+++ demo12/src/demo12/Add.java
@@ -0,0 +1,13 @@
+package demo12;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=4,b=5,c;
+		c=a+b;
+		System.out.println("adding two numbers"+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
